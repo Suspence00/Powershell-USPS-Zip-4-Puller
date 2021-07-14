d1862c81e2807d4cbedb6dc332fd8ad3b0521521
Spencer <SpencerSchilsky@gmail.com>
2021-07-14 04:33:51 -0400

    Total Rewrite
    add validation and parsing through proper API
    added comments

--- poshuspszip4.ps1
+++ poshuspszip4.ps1
@@ -1,23 +1,51 @@
 #Code by Spencer
 #7.14.2021
 #Powerhsell Tool to utilize USPS Web Tools API for Zip+4 Information
+# 1. Validates address via Web Request
+# 2. Queries for Zip4 based on validated information
 
+# User must obtain a valid User ID for USPS's Web Tools API by registering through USPS's website. Information for that can be found here: https://www.usps.com/business/web-tools-apis/
+$USERID = ""
 
-#Collects Address Information
-$USERID = Read-Host -Prompt "Enter the USPS Web Tools API UserID/Password"
-$Address2 = Read-Host -Prompt "Street Address:"
-$Address1 = Read-Host -Prompt "(optional) Street Address 2"
-$City = Read-Host -Prompt "City"
-$State =  Read-Host -Prompt "State"
-$Zip5 =  Read-Host -Prompt "Zip+5"
+#Enter Address Information
+$Address1 = "211 NE Revere Ave" 
+$Address2 = ""
+$City = "Bend"
+$State = "OR"
+$Zip5 = "97701"
 
-#Adds Quotes around USERID - needed
-$USERID = '"'+$USERID+'"'
+#Adds Quotes around USERID - needed or it breaks. Also checks to make sure it's not blank.
+if ($USERID -eq ""){
+	Write-Host "No User ID Provided, please modify script before running" -ForegroundColor Red
+	break;}
+	
+	else{$USERID = '"'+$USERID+'"'}	
+		
 # Must be 0 as per Docs
-$addressID = '"0"'	
+$addressID = '"0"'
+
 #Request Address
-$r ="http://production.shippingapis.com/ShippingAPI.dll?API=Verify&XML=<AddressValidateRequest USERID=$USERID><Address ID=$addressID><Address1>$Address1</Address1><Address2>$Address2</Address2><City>$City</City><State>$State</State><Zip5>$Zip5</Zip5><Zip4></Zip4></Address></AddressValidateRequest>" 
-$g = Invoke-WebRequest $r
-#Pulls information from request by casting data as xml
-Write-Host "`n Full Address with Zip+4:"
-([xml]$g).AddressValidateResponse.Address
+$validateRequestURI ="https://secure.shippingapis.com/ShippingAPI.dll?API=Verify&XML=<AddressValidateRequest USERID=$USERID><Address ID=$addressID><Address1>$Address1</Address1><Address2>$Address2</Address2><City>$City</City><State>$State</State><Zip5>$Zip5</Zip5><Zip4></Zip4></Address></AddressValidateRequest>" 
+$validatedAddress = Invoke-WebRequest $validateRequestURI
+
+#Modifies variables to validated data 
+$Address2 = ([xml]$validatedAddress).AddressValidateResponse.Address.Address2
+$Address1 =([xml]$validatedAddress).AddressValidateResponse.Address.Address1
+$City =([xml]$validatedAddress).AddressValidateResponse.Address.City
+$State =([xml]$validatedAddress).AddressValidateResponse.Address.State
+$Zip5 =([xml]$validatedAddress).AddressValidateResponse.Address.Zip5
+
+#zip4 API request
+$zip4URI ="https://secure.shippingapis.com/ShippingAPI.dll?API=ZipCodeLookup&XML=<ZipCodeLookupRequest USERID=$USERID><Address ID=$addressID><Address1>$Address1</Address1><Address2>$Address2</Address2><City>$City</City><State>$State</State><Zip5>$Zip5</Zip5><Zip4></Zip4></Address></ZipCodeLookupRequest>" 
+$zip4Request = Invoke-WebRequest $zip4URI	
+$zip4 = ([xml]$zip4Request).ZipCodeLookupResponse.Address.Zip4
+
+#final output
+Write-Host "The address has been Validated, pulling Zip+4..."  -ForegroundColor Blue
+Write-Host "Full Inforomation:" -ForegroundColor Blue
+Write-Host "Address 1: $Address2" -ForegroundColor Yellow
+Write-Host "Address 2: $Address1" -ForegroundColor Yellow
+Write-Host "City: $City" -ForegroundColor Yellow
+Write-Host "State 2: $State" -ForegroundColor Yellow
+Write-Host "Zip5 2: $Zip5" -ForegroundColor Yellow
+Write-Host "Zip4: $Zip4" -ForegroundColor Green
