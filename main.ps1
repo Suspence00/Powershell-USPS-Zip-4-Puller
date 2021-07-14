565582d9081ccf2b097b6371004537f2d02a0227
Spencer <SpencerSchilsky@gmail.com>
2021-07-14 01:26:36 -0400

    Formatting Changes

--- main.ps1
+++ main.ps1
@@ -4,12 +4,12 @@
 
 
 #Collects Address Information
-$USERID = Read-Host -Prompt "Enter the USPS Web Tools API UserID/Password "
-$Address2 = Read-Host -Prompt "Street Address: "
-$Address1 = Read-Host -Prompt "(optional) Street Address 2 "
-$City = Read-Host -Prompt "City "
-$State =  Read-Host -Prompt "State "
-$Zip5 =  Read-Host -Prompt "Zip+5 "
+$USERID = Read-Host -Prompt "Enter the USPS Web Tools API UserID/Password"
+$Address2 = Read-Host -Prompt "Street Address:"
+$Address1 = Read-Host -Prompt "(optional) Street Address 2"
+$City = Read-Host -Prompt "City"
+$State =  Read-Host -Prompt "State"
+$Zip5 =  Read-Host -Prompt "Zip+5"
 
 #Adds Quotes around USERID - needed
 $USERID = '"'+$USERID+'"'
@@ -19,6 +19,5 @@ $addressID = '"0"'
 $r ="http://production.shippingapis.com/ShippingAPI.dll?API=Verify&XML=<AddressValidateRequest USERID=$USERID><Address ID=$addressID><Address1>$Address1</Address1><Address2>$Address2</Address2><City>$City</City><State>$State</State><Zip5>$Zip5</Zip5><Zip4></Zip4></Address></AddressValidateRequest>" 
 $g = Invoke-WebRequest $r
 #Pulls information from request by casting data as xml
-([xml]$g).AddressValidateResponse.Address
-Write-Host "Full Address with Zip+4:"
+Write-Host "`n Full Address with Zip+4:"
 ([xml]$g).AddressValidateResponse.Address
