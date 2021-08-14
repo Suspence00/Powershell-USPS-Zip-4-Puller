bba422d4a9673dd287ffc1c900396181d01e80ac
Spencer <SpencerSchilsky@gmail.com>
2021-08-13 22:32:11 -0400

    Added XML/Account and Address Error Checking
    Added error checking for invalid account/bad xml and if the address is entered incorrectly/isn't found.

--- poshuspszip4.ps1
+++ poshuspszip4.ps1
@@ -1,5 +1,5 @@
 #Code by Spencer
-#7.14.2021
+#8.13.2021
 #Powershell Tool to utilize USPS Web Tools API for Zip+4 Information
 # 1. Validates address via Web Request
 # 2. Queries for Zip4 based on validated information
@@ -10,16 +10,17 @@ $USERID = ""
 
 #Enter Address Information
 #Note: Address2 is the main street address, Address1 is the Suite/Apartment
-$Address2 = "211 NE Revere Ave" 
+$Address2 = "700 State Highway" 
 $Address1 = ""
-$City = "Bend"
-$State = "OR"
-$Zip5 = "97701"
+$City = "Lewisville"
+$State = "TX"
+$Zip5 = "75067"
 
 # Adds Quotes around USERID - needed or it breaks. Also checks to make sure it's not blank.
 if ($USERID -eq ""){
 	Write-Host "No User ID Provided, please modify script before running" -ForegroundColor Red
-	break;}
+	break;
+	}
 	
 $USERID = '"'+$USERID+'"'
 
@@ -30,6 +31,30 @@ $addressID = '"0"'
 $validateRequestURI ="https://secure.shippingapis.com/ShippingAPI.dll?API=Verify&XML=<AddressValidateRequest USERID=$USERID><Address ID=$addressID><Address1>$Address1</Address1><Address2>$Address2</Address2><City>$City</City><State>$State</State><Zip5>$Zip5</Zip5><Zip4></Zip4></Address></AddressValidateRequest>" 
 $validatedAddress = Invoke-WebRequest $validateRequestURI
 
+#Checks for any Errors, Prints Error and Ends Script
+
+#Account/XML Failure Error Check
+if (([XML]$validatedAddress).Error){
+	Write-Host "The following error was encountered:" -ForegroundColor Red
+	Write-Host "Error Number:" $([XML]$validatedAddress).Error.Number -ForegroundColor Red
+	Write-Host "Error Description:" $([XML]$validatedAddress).Error.Description -ForegroundColor Red
+	Write-Host "Error Source:" $([XML]$validatedAddress).Error.Source -ForegroundColor Red
+	Write-Host "Please make sure the account saved in the script is valid and then retry." -ForegroundColor Red
+	break;
+	}
+
+#Address Error Check
+if (([XML]$validatedAddress).AddressValidateResponse.Address.Error){
+	Write-Host "The entered address presented the following error:" -ForegroundColor Red
+	Write-Host "Error Number:" $([XML]$validatedAddress).AddressValidateResponse.Address.Error.Number -ForegroundColor Red
+	Write-Host "Error Source:" $([XML]$validatedAddress).AddressValidateResponse.Address.Error.Source -ForegroundColor Red
+	Write-Host "Error Description:" $([XML]$validatedAddress).AddressValidateResponse.Address.Error.Description -ForegroundColor Red
+	Write-Host "Help File (if Available):" $([XML]$validatedAddress).AddressValidateResponse.Address.Error.HelpFile -ForegroundColor Yellow
+	Write-Host "Help Context (if Available):" $([XML]$validatedAddress).AddressValidateResponse.Address.Error.HelpContext -ForegroundColor Yellow
+	Write-Host "Please make sure the address saved in the script is valid and then retry." -ForegroundColor Red
+	break;
+	}
+	
 # Modifies variables to validated data 
 $Address2 = ([xml]$validatedAddress).AddressValidateResponse.Address.Address2
 $Address1 = ([xml]$validatedAddress).AddressValidateResponse.Address.Address1
